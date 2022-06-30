be6486d5f5b625d938faceabeefe7c6292508a95
jubaer ibn zohir <jubaeribnzohir@gmail.com>
2022-07-01 02:10:42 +0600

    update and delete done

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ app.use(express.json());
 const PORT = process.env.PORT || 5500;
 
 //use cors
-// app.use(cors());
+app.use(cors());
 
-// //import routes
-// const TodoItemRoute = require('./routes/todoItems');
-
-
-// //connect to mongodb ..
-// mongoose.connect(process.env.DB_CONNECT)
-// .then(()=> console.log("Database connected"))
-// .catch(err => console.log(err))
+//import routes
+const TodoItemRoute = require('./routes/todoItems');
 
 
-// app.use('/', TodoItemRoute);
+//connect to mongodb ..
+mongoose.connect(process.env.DB_CONNECT)
+.then(()=> console.log("Database connected"))
+.catch(err => console.log(err))
+
+
+app.use('/', TodoItemRoute);
 
 
 
